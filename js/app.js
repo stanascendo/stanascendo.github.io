3fffcac42d2e79e2e30cf3f0e6d2dc0549a6966a
Tommaso <p.tommy93@gmail.com>
2016-02-10 14:30:18 +0100

    Improve layout. Numbers are now correctly padded with 0 if needed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#clock').countdown('2017/02/11', function(event) {
-        $(this).html(event.strftime('%D %!D:giorno,giorni;<br />%H %!H:ora,ore;<br />%M %!M:minuto,minuti;<br />%S %!S:secondo,secondi;<br />'));
+        $(this).html(event.strftime('%-D %!D:giorno,giorni;<br />%-H %!H:ora,ore;<br />%-M %!M:minuto,minuti;<br />%-S %!S:secondo,secondi;<br />'));
     });
 });
