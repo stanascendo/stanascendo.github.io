21b02b19b4c8eeb76b62fd40995b5b2cc1d24f0f
Tommaso <p.tommy93@gmail.com>
2016-02-10 15:16:49 +0100

    Length mistake

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#clock').countdown('2017/02/11', function(event) {
+    $('#clock').countdown('2016/04/10', function(event) {
         $(this).html(event.strftime('%-D %!D:giorno,giorni;<br />%-H %!H:ora,ore;<br />%-M %!M:minuto,minuti;<br />%-S %!S:secondo,secondi;<br />'));
     });
 });
